79b558d2c8326b697469c5173f92a6cb987af95f
Steffen Müthing <muething@dune-project.org>
2013-04-09 17:28:41 +0200

    Make fossil id extraction more robust for e.g. monster merge commits

--- add-fossil-ids.py
+++ add-fossil-ids.py
@@ -4,7 +4,6 @@ import sys, re, os, tempfile
 
 EVENT_DELIMITER = '------------------------------------------------------------------------------\n'
 
-
 def event_splitter(s):
     # remove initial delimiter
     s.readline()
@@ -26,14 +25,24 @@ with open(sys.argv[1],'r') as input, tempfile.NamedTemporaryFile(delete=False) a
     tmp_name = output.name
 
     for event in event_splitter(input):
-        svn_id = event[5].split()[1].strip()
+        # locate SVN id and generate string with backreference
+        for line in event:
+            if len(line.strip()) == 0:
+                # whoops, no fossil id found, report that in the commit message
+                msg = '\n[[No corresponding SVN revision found]]\n'
+                break
+            if line.startswith('Fossil-ID:'):
+                # Jackpot!
+                svn_id = line.split()[1].strip()
+                msg = '\n[[Imported from SVN: r{}]]\n'.format(svn_id)
+                break
 
         output.write(EVENT_DELIMITER)
 
         for line in event:
             output.write(line)
 
-        output.write('\n[[Imported from SVN: r{}]]\n'.format(svn_id))
+        output.write(msg)
 
 # replace original with edited file
 os.rename(tmp_name,sys.argv[1])
